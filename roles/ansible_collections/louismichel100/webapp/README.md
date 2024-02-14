f2235714b7d50c036646303d77a63860771743f4
sangala mballa <sangalamballamichel@gmail.com>
2024-02-14 13:25:04 +0000

    suppress comment

--- roles/ansible_collections/louismichel100/webapp/README.md
+++
@@ -1,4 +0,0 @@
-# Ansible Collection - louismichel100.webapp
-
-Documentation for the collection.
-# ansible-collection_webapp
--- roles/ansible_collections/louismichel100/webapp/roles/nginx/systemd/src/basic/missing_wait.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later */
-#pragma once
-
-#include <sys/wait.h>
-
-#ifndef P_PIDFD
-#define P_PIDFD 3
-#endif
